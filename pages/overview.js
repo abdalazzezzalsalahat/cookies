9da652df0afdc1fb78d31175183b469bbca26515
abdalazez <abdalazzezzalsalahat@icloud.com>
2021-07-07 19:16:40 +0200

    all new up and running

--- pages/overview.js
+++ pages/overview.js
@@ -1,11 +1,11 @@
-import Head from '../components/CookieStandHead'
-import Header from '../components/CookieStandHeader'
+import Head from '../components/CookiesHead'
+import Header from '../components/CookiesHeader'
 import { useState } from 'react'
 
 export default function Overview() {
     const [title,setTitle] = useState('Overview');
     const [path,setPath] = useState("/")
-    const [page,setPage] = useState("Cookie Stand Admin")
+    const [page,setPage] = useState("Cookie Admin")
 
     return (
         <div className="bg-green-100">
